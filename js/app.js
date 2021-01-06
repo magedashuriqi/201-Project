5606f8b50f9a8059341bc8d1a8e865421bcf63c9
mariammohamme <mariam.mohammed942@hotmail.com>
2021-01-06 17:28:10 +0200

    change by mariam

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var score=0;
 var userName = prompt("what is your name?")
 confirm("Hi " + userName + ", now we will do a small guess, are you ready?")
 
@@ -13,7 +13,7 @@ if (myCountry.toLowerCase() == 'y' || myCountry.toLowerCase() == 'yes') {
 } else if (myCountry.toLowerCase() == 'n' || myCountry.toLowerCase() == 'no') {
     alert('That is correct!, Iam from syria ^^')
     //console.log("That is correct!, Iam from syria")
-    myCountry = 1
+   score++;
 } else {
     alert('Sorry, your input is not valied!')
     myCountry = 0
@@ -41,10 +41,10 @@ if (myCountry.toLowerCase() == 'y' || myCountry.toLowerCase() == 'yes') {
 */
 // second question
 function guessExperiance(){
-    var myExperience = prompt("Do you think that I work before?")
+var myExperience = prompt("Do you think that I work before?")
 if (myExperience.toLowerCase() == 'y' || myExperience.toLowerCase() == 'yes') {
     alert('That is right!, you will know more in the website ^^')
-    myExperience = 1
+    score++;
     //console.log("That is right!, you will know more in the website ^^")
 } else if (myExperience.toLowerCase() == 'n' || myExperience.toLowerCase() == 'no') {
     alert('wrong answer !^^')
@@ -54,7 +54,7 @@ if (myExperience.toLowerCase() == 'y' || myExperience.toLowerCase() == 'yes') {
     alert('Sorry, your input is not valied!')
     myExperience = 0
 }
-return myExperience();
+return myExperience;
 }
 guessExperiance();
 /*
@@ -73,6 +73,27 @@ if (myExperience.toLowerCase() == 'y' || myExperience.toLowerCase() == 'yes') {
 }
 //console.log("Sorry, your input is not valied!"")
 */
+// question three
+function gueesfavColor(){
+    var favColor = prompt("Do you think that I love green?")
+if (favColor.toLowerCase() == 'y' || favColor.toLowerCase() == 'yes') {
+    alert('wrong answer !^^')
+    favColor = 0
+    //console.log("wrong answer !^^")
+} else if (favColor.toLowerCase() == 'n' || favColor.toLowerCase() == 'no') {
+    alert('That is right!')
+    score++;
+    //console.log("That is right!")
+} else {
+    alert('Sorry, your input is not valied!')
+    favColor = 0;   
+}
+console.log(favColor);
+return favColor;
+}
+gueesfavColor();
+
+/*
 var favColor = prompt("Do you think that I love green?")
 if (favColor.toLowerCase() == 'y' || favColor.toLowerCase() == 'yes') {
     alert('wrong answer !^^')
@@ -87,7 +108,26 @@ if (favColor.toLowerCase() == 'y' || favColor.toLowerCase() == 'yes') {
     favColor = 0
 }
 //console.log("Sorry, your input is not valied!")
-
+*/
+// question four
+function guessHobbies(){
+    var myHobbies = prompt("Do you think that I can draw?")
+if (myHobbies.toLowerCase() == 'y' || myHobbies.toLowerCase() == 'yes') {
+    alert('That is right!')
+    score++;
+    //console.log ("That is right!")
+} else if (myHobbies.toLowerCase() == 'n' || myHobbies.toLowerCase() == 'no') {
+    alert('wrong answer !^^')
+    myHobbies = 0
+    //console.log ("wrong answer !^^")
+} else {
+    alert('Sorry, your input is not valied!')
+    myHobbies = 0
+}
+return myHobbies;
+}
+guessHobbies();
+/*
 var myHobbies = prompt("Do you think that I can draw?")
 if (myHobbies.toLowerCase() == 'y' || myHobbies.toLowerCase() == 'yes') {
     alert('That is right!')
@@ -102,7 +142,26 @@ if (myHobbies.toLowerCase() == 'y' || myHobbies.toLowerCase() == 'yes') {
     myHobbies = 0
 }
 //console.log ("Sorry, your input is not valied!")
-
+*/
+// question five
+function gueesAge(){
+    var myAge = prompt("Do you think that I was born in 1997?")
+if (myAge.toLowerCase() == 'y' || myAge.toLowerCase() == 'yes') {
+    alert('That is right! ' + userName)
+    score++;
+    //console.log ("That is right!")
+} else if (myAge.toLowerCase() == 'n' || myAge.toLowerCase() == 'no') {
+    alert('wrong answer !^^ ' + userName)
+    myAge = 0
+    //console.log ("wrong answer !^^")
+} else {
+    alert('Sorry, your input is not valied! ' + userName)
+    myAge = 0
+}
+return myAge;
+}
+gueesAge();
+/*
 var myAge = prompt("Do you think that I was born in 1997?")
 if (myAge.toLowerCase() == 'y' || myAge.toLowerCase() == 'yes') {
     alert('That is right! ' + userName)
@@ -117,10 +176,38 @@ if (myAge.toLowerCase() == 'y' || myAge.toLowerCase() == 'yes') {
     myAge = 0
 }
 //console.log ("Sorry, your input is not valied!")
+*/
 
 
-
-
+// question six
+function gueesNumber()
+{
+alert('You will start a guess game')
+var i = 0;
+var answer = '7';
+var userGuess;
+while (i < 4) {
+    userGuess = parseInt(prompt('guess a nubmer between 1 & 10'))
+    if (answer > userGuess) {
+        alert('too low')
+    } else if (answer < userGuess) {
+        alert('too high')
+    } else if (answer == userGuess) {
+        alert('great!!. correct answer')
+        break;
+    }
+    i++;
+}
+if (userGuess === 7) {
+    score++;
+} else {
+    userGuess = 0
+}
+alert('The correct answer is 7 <3')
+return userGuess;
+}
+gueesNumber();
+/*
 alert('You will start a guess game')
 var i = 0;
 var answer = '7';
@@ -143,7 +230,30 @@ if (userGuess === 7) {
     userGuess = 0
 }
 alert('The correct answer is 7 <3')
-
+*/
+// question saven
+function gueesColor(){
+var a = 0;
+var colors = ["red", "blue", "pink", "grey", "green", "black"];
+var colorGuess;
+while (a < 6) {
+    colorGuess = (prompt('guess a color'))
+    if (colorGuess.toLowerCase() == colors[0] || colorGuess.toLowerCase() == colors[1] || colorGuess.toLowerCase() == colors[2] || colorGuess.toLowerCase() == colors[3] || colorGuess.toLowerCase() == colors[4] || colorGuess.toLowerCase() == colors[5]) {
+        alert('correct! <3')
+        score++;
+        break;
+    } else {
+        alert('wrong answer :P')
+        colorGuess = 0
+    }
+    a++;
+    console.log(colorGuess);
+}
+return colorGuess;
+}
+gueesColor();
+alert('total score='+ score);
+/*
 var a = 0;
 var colors = ["red", "blue", "pink", "grey", "green", "black"];
 var colorGuess;
@@ -160,5 +270,6 @@ while (a < 6) {
     a++;
     console.log(colorGuess);
 }
+*/
 
-alert('your score is ' + (userGuess + myAge + myCountry + myExperience + colorGuess + myHobbies + favColor) + '/7')
+//alert('your score is ' + (userGuess + myAge + myCountry + myExperience + colorGuess + myHobbies + favColor) + '/7')
